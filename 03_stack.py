4d0a5cdc8d7ba19e9ae345fe97dbd275a88c3760
Mukesh61 <45516247+Mukesh61@users.noreply.github.com>
2025-03-03 00:29:09 +0530

    Create 03_stack.py

---
+++ 03_stack.py
@@ -0,0 +1,18 @@
+'''
+insert and delete from top of the stack. 
+
+LIFO : last in first out
+
+1. push
+2. pop
+3. peek: return top item without deleting it. like just to check
+4. is_empty: return true if the stack is empty. 
+
+Usage: 
+1. Syntax parsing
+2. used in recursion
+3. Undo and redo operations in word processors. 
+4. low level assembly programming. 
+
+'''
+
