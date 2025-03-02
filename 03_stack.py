41f92b9d47f2d7240b463ae902e085e2786893c7
Mukesh61 <45516247+Mukesh61@users.noreply.github.com>
2025-03-03 01:19:23 +0530

    Update 03_stack.py

--- 03_stack.py
+++ 03_stack.py
@@ -16,3 +16,38 @@ Usage:
 
 '''
 
+class CustomStack:
+    def __init__(self):
+        self._internal_list = []
+
+    def is_empty(self):
+        if not len(self._internal_list):
+            return True
+        return False
+
+    def push(self, item):
+        self._internal_list.append(item)
+
+    def pop(self):
+        return self._internal_list.pop()
+
+    def peek(self):
+        if not self.is_empty():
+            return self._internal_list[-1]
+
+
+if __name__ == '__main__':
+    stack = CustomStack()
+
+    print(stack.is_empty())
+
+    print("add 1 and 2 to stack")
+    stack.push(1)
+    stack.push(2)
+
+    print("peek the item")
+    print(stack.peek())
+
+    print("remove one item")
+    print(stack.pop())
+
